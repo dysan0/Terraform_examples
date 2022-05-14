10f0f739f16cbee500d08288808278ba06269868
user <email@example.com>
2022-05-14 09:42:14 -0300

    Initial Commit

---
+++ chapter2/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-0c55b159cbfafe1f0"
+    instance_type = "t2.micro"
+    
+    tags = {
+        #Instance Name
+        Name = "terraform-example3"
+    }
+}
